7e418caf5c1b9d8bca2d439633b35e5921225f36
Michal Hromas <michalhromas1@gmail.com>
2021-07-06 18:35:44 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `ahoj`,
 })
-export class AppComponent {
-  title = 'reactive-todolist';
-}
+export class AppComponent {}
